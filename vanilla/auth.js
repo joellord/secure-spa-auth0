3a3d36e973bcdb54612db1d54e43a7aa18ec8b15
Joel Lord <joelphy@gmail.com>
2018-03-13 12:47:03 -0400

    Now uses Auth0 too

--- vanilla/auth.js
+++ vanilla/auth.js
@@ -1,6 +1,5 @@
 const ACCESS_TOKEN = "access_token";
 const AUTH_URL = "https://wt-13aebf4eeaa9913542725d4a90e4d49e-0.run.webtask.io/auth-server-1";
-const AUTH_AUTH0_URL = "https://wt-13aebf4eeaa9913542725d4a90e4d49e-0.run.webtask.io/clickbaiter-auth0";
 
 let auth = {};
 auth.useAuth0 = false;
@@ -36,12 +35,12 @@ auth.loginTraditional = () => {
       });
 };
 
-var webAuth = new auth0.WebAuth({
+let webAuth = new auth0.WebAuth({
   domain: 'joel-1.auth0.com',
-  clientID: 'NqF5L8KTz1XjZkWjd3ZR6i7qkfjJuCDR',
+  clientID: 'UF5d0t8oFPhWQLkzkLu192FW9WH8jK0k',
   responseType: 'token id_token',
-  audience: 'https://joel-1.auth0.com/userinfo',
-  scope: 'openid',
+  audience: 'secure-spa-auth0',
+  scope: 'openid profile',
   redirectUri: window.location.href
 });
 
@@ -50,5 +49,19 @@ auth.loginAuth0 = () => {
   return Promise.resolve();
 };
 
+auth.parseHash = () => {
+  let hash = window.location.hash.substr(0,1) == "#" ? window.location.hash.substr(1) : window.location.hash;
+  let queryParams = {};
+  hash.split("&").map(param => {
+    param = param.split("=");
+    queryParams[param[0]] = param[1];
+  });
+  if (queryParams.access_token && queryParams.expires_in) {
+    localStorage.setItem(ACCESS_TOKEN, queryParams.access_token);
+    UIUpdate.loggedIn();
+    UIUpdate.alertBox("Logged in<br>Access Token: " + queryParams.access_token + "<br>ID Token: " + queryParams.id_token);
+  }
+  window.location.hash = "";
+};
 
-
+window.addEventListener("DOMContentLoaded", auth.parseHash);
